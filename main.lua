5bc1f31599b1b89433171e8bf2bd9b02ade724a8
Taito <3835littletaitomonster@gmail.com>
2024-06-20 21:31:02 +0900

    test commit

--- main.lua
+++ main.lua
@@ -1,20 +1,22 @@
 -- Load some default values for our rectangle.
-function love.load()
-    x, y, w, h = 20, 20, 60, 20
-end
+-- function love.load()
+--     x, y, w, h = 20, 20, 60, 20
+-- end
 
 -- Increase the size of the rectangle every frame.
-function love.update(dt)
-    w = w + 1
-    h = h + 1
-end
+-- function love.update(dt)
+--     w = w + 1
+--     h = h + 1
+-- end
 
 -- Draw a coloured rectangle.
-function love.draw()
+-- function love.draw()
     -- In versions prior to 11.0, color component values are (0, 102, 102)
-    love.graphics.setColor(0, 0.4, 0.4)
-    love.graphics.rectangle("fill", x, y, w, h)
-    function love.load(
+    -- love.graphics.setColor(0, 0.4, 0.4)
+    -- love.graphics.rectangle("fill", x, y, w, h)
+    -- end
+
+    function love.load()
       animation=newAnimation(love.graphics.newImage("oldHero.png"),16,18,1)
     end
   function love.update(dt)
